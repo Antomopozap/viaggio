5d5621cda008442c642a3cc1c926558ac88f70d8
Antomopozap <85046221+Antomopozap@users.noreply.github.com>
2024-04-10 20:30:44 +0200

    yes

--- src/js/misc.js
+++ src/js/misc.js
@@ -1,8 +1,11 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    $("#container").load(`./src/pages/page0.html`);
-    // document.getElementById("active-bar").style.opacity = "0";
-  },
-  false
-);
+//!Popup
+function funcpopup() {
+  $("#container").load(`./src/pages/page0.html`);
+  activeNav.style.opacity = "0";
+  gsap.to(links, { color: "#FFF" });
+  const popup = document.getElementById("popup");
+  gsap.to(popup, 0.5, { opacity: 1 });
+  setTimeout(() => {
+    gsap.to(popup, 0.5, { opacity: 0 });
+  }, 1500);
+}
