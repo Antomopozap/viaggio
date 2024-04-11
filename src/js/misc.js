dff039c9e7af32b73ca46cfaff2a20ae2ae9a1a4
Antomopozap <85046221+Antomopozap@users.noreply.github.com>
2024-04-11 20:49:48 +0200

    si

--- src/js/misc.js
+++ src/js/misc.js
@@ -1,3 +1,6 @@
+//!Load up main page
+$("#container").load(`./src/pages/page0.html`);
+
 //!Popup
 function funcpopup() {
   $("#container").load(`./src/pages/page0.html`);
@@ -9,8 +12,27 @@ function funcpopup() {
     gsap.to(popup, 0.5, { opacity: 0 });
   }, 1500);
 }
-//!Cambio di titolo
+
+//?DEATH
+function death() {
+  $("#container").load(`./src/pages/death.html`);
+}
+
 setTimeout(() => {
-  newPageTitle = "Agenzia Viaggi";
-  document.title = newPageTitle;
-}, 5000);
+  $("#container").load(`./src/pages/page0.html`);
+}, 100);
+
+//!death
+
+function morte(n) {
+  const selected = document.getElementById(`pag${n}`);
+  const first = document.getElementById(`pag1`);
+  const second = document.getElementById(`pag2`);
+  const third = document.getElementById(`pag3`);
+  const fourth = document.getElementById(`pag4`);
+  gsap.to(first, 0.1, { opacity: 0 });
+  gsap.to(second, 0.1, { opacity: 0 });
+  gsap.to(third, 0.1, { opacity: 0 });
+  gsap.to(fourth, 0.1, { opacity: 0 });
+  gsap.to(selected, 0.1, { opacity: 1 });
+}
